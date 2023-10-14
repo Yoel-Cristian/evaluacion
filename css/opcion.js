bd55104279301a0b799af600212521bef1edea22
Yoel-Cristian-Quispe-Diaz <e117303@uajms.edu.bo>
2023-10-13 21:32:25 -0400

    corregido

--- css/opcion.js
+++ css/opcion.js
@@ -1,20 +1,6 @@
 
 function cargarNuevaPagina(r) {
-closeNav();
-
-  var elementos = document.querySelectorAll(".Formulario");
-
-  // Oculta todos los divs
-  elementos.forEach(function (elemento) {
-    elemento.style.display = "none";
-  });
-
-
-  
   var iframe = document.getElementById("miFrame");
   iframe.src = r;
   iframe.style.display = "block";
-}
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
 }
